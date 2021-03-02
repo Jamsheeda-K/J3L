c16ae1479288185b76ce562ff18234f957daa683
Jens Krause <jxkrause@posteo.de>
2021-03-02 15:11:40 +0100

    customer class

---
+++ notebooks/jens/customer.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import random
+
+class Customer:
+    """a single customer that moves through the supermarket in a MCMC simulation"""
+
+    def __init__(self, id, transition_mat):
+        self.id = id
+        self.state = 'in'
+        self.transition_mat = transition_mat
+        self.count = 0
+
+    def __repr__(self):
+        """
+        Returns a csv string for that customer.
+        """
+        return f'{self.id}:{self.state}'
+
+    def is_active(self):
+        """
+         Returns True if the customer has not reached the checkout
+         for the second time yet, False otherwise.
+        """
+        return self.state!='out'
+
+    def next_state(self):
+        """
+        Propagates the customer to the next state
+          using a weighted random choice from the transition probabilities
+        conditional on the  current state.
+          Returns nothing.
+        """
+        row = self.transition_mat.loc[self.state]
+        nstate = random.choices(self.transition_mat.columns, row)
+        self.state = nstate[0]
+        self.count += 1
+
+if(__name__=='__main__'):
+    df_transition = pd.read_csv('data/freq_table.csv', index_col=0)
+    #print(df_transition)
+    for i in range(12):
+        p1 = Customer(i,df_transition)
+        print(p1)
+        while(p1.is_active()):
+            p1.next_state()
+            print(p1)
--- notebooks/jens/probas.py
+++ notebooks/jens/probas.py
@@ -6,11 +6,12 @@ df = load_supermarket_data('data')
 
 locations = list(df['location'].unique())
 locations.append('out')
+locations.append('in')
+
 
 dfg = df.groupby([df.index.weekday,'customer_no'],as_index=False)
 
 freq_table = pd.DataFrame(0, index=locations, columns=locations, dtype=int)
-init_series = pd.Series(0, index=locations, dtype=int)
 
 # slow: takes 5 min on my computer
 for x in tqdm.tqdm(dfg):
@@ -18,7 +19,7 @@ for x in tqdm.tqdm(dfg):
     dfcross = pd.crosstab(cstm, cstm.shift(-1))
     freq_table.loc[dfcross.index,dfcross.columns] += dfcross
     freq_table.loc[cstm.iloc[-1],'out'] += 1
-    init_series.loc[cstm.iloc[1]] += 1
+    freq_table.loc['in',cstm.iloc[0]] += 1
 
 freq_table.loc['out','out'] = 1
 freq_table.to_csv('data/freq_table.csv')
