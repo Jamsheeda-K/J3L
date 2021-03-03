65f02cf40457d980dd7d06e46056bead7d207e79
Jamsheeda-K <jamsheedak@gmail.com>
2021-03-03 23:21:27 +0100

    corrected creating transition matrix

--- notebooks/jamsheeda/create_transition_matrix.py
+++ notebooks/jamsheeda/create_transition_matrix.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 import numpy as np
 import os
+
 # Loading the customer movement data for 5 days
 directory_in_str = "./data/supermarket/"
 directory = os.fsencode(directory_in_str)
@@ -11,27 +12,27 @@ for filename in os.listdir(directory):
     data.append(df_temp)
 df = pd.concat(data)
 
-#Transforming the dataframe for better implementation
-transformed_df = df.reset_index().groupby(['customer_no','week_day'])[['timestamp','location']].agg(lambda x:list(x))
+# to capture the time factor also we need to regularise to 1 min interval
+new_df= df.groupby(['customer_no','week_day'])['location'].resample('1T').ffill()
+
+# transforming matrix
+transformed_df = new_df.reset_index().groupby(['customer_no','week_day'])[['timestamp','location']].agg(lambda x:list(x))
 
 #creating transition matrix with before and after states
 transition_df=pd.DataFrame(columns=['before','after'])
+index = 0
 for i in transformed_df.iterrows():
     customer_no = i[0][0]
     week_day = i[0][1]
     time_stamp = i[1][0]
     sections = i[1][1]
-    #print(sections)
     sections.insert(0,'entrance')
-    time_stamp.insert(0,time_stamp[0])
-    #print(sections)
-    #print(time_stamp)
+    entrance_time = pd.to_datetime(time_stamp[0]) + pd.to_timedelta(-1, unit='m')
+    time_stamp.insert(0,entrance_time)
     for j in range(len(sections)):
         if (j+1)<len(sections):
-            transition_df.loc[time_stamp[j]]=(sections[j],sections[j+1])
-
-#resampling data to fill in every minute
-resampled_df = transition_df.resample('1T').ffill()
+            transition_df.loc[index]=(sections[j],sections[j+1])
+            index += 1
 
 #creating transition probability matrix
 transition_probability_matrix = pd.crosstab(transition_df['before'], transition_df['after'], normalize=0)
@@ -40,6 +41,8 @@ transition_probability_matrix.loc['checkout'] = [1,0,0,0,0,0]
 
 # ordering the columns inside
 transition_probability_matrix = pd.DataFrame(transition_probability_matrix,columns=['entrance','dairy','drinks','fruit','spices','checkout'],index=['entrance','dairy','drinks','fruit','spices','checkout'])
-#print(transition_probability_matrix)
+assert all(transition_probability_matrix.sum(axis=1) > 0.999)
+print(transition_probability_matrix)
 
-
+# save to csv file
+transition_probability_matrix.to_csv('transition_matrix.csv')
