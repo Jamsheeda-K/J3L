fd7b28e88cc9f0b47fe3649a5774ca6200749696
Jens Krause <jxkrause@posteo.de>
2021-03-05 09:56:43 +0100

    finishing touches

--- src/load_data.py
+++ src/load_data.py
@@ -4,14 +4,16 @@
 #reading data from supermarket
 
 import pandas as pd
-import os
+#import os
+
+files = [ 'monday.csv', 'tuesday.csv', 'friday.csv',      'thursday.csv',  'wednesday.csv' ]
 
 def load_supermarket_data(data_dir):
     "read shop data from folder 'data_dir' into one pandas dataframe"
     dfs = []
-    for x in os.listdir(data_dir):
-        if(x=='freq_table.csv' | x=='supermarket.csv'): 
-            continue
+    for x in files:
+        #if(x=='freq_table.csv' | x=='supermarket.csv'): 
+        #    continue
         df = pd.read_csv(data_dir + '/' + x, sep=';', parse_dates=True, infer_datetime_format=True)
         dfs.append(df)
     df = pd.concat(dfs)
