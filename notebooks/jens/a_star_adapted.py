82fee28ea49aca535016ebdca94de5a7e23441c5
Jens Krause <jxkrause@posteo.de>
2021-03-05 15:07:08 +0100

    fix simulation

--- notebooks/jens/a_star_adapted.py
+++ notebooks/jens/a_star_adapted.py
@@ -1,5 +1,8 @@
-from src.a_star import find_path
+"""
+Adpapt Gesa's A star algorithm to the supermarkte simulation
+"""
 import numpy as np
+from src.a_star import find_path
 
 MARKET = """
 ##################
@@ -17,22 +20,21 @@ MARKET = """
 """.strip()
 
 
-class A_star_supermarket:
+class AstarSupermarket:
+    "Adaptor between superamarket and Astar"
     def __init__(self):
         contents = [list(row) for row in MARKET.split("\n")]
-        nr = len(contents)
-        nc = len(contents[0])
-        self.grid = np.zeros((nr,nc),int)
-        for i in range(nr):
-            for j in range(nc):
+        n_r = len(contents)
+        n_c = len(contents[0])
+        self.grid = np.zeros((n_r, n_c),int)
+        for i in range(n_r):
+            for j in range(n_c):
                 if(contents[i][j] not in ['G', '.']):
                     self.grid[i,j] = 1
-        #print(self.grid)
         self.p_moves = [(0,1),(0,-1),(1,0),(-1,0)]
         #self.p_moves = [(0,1),(0,-1),(1,0),(-1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
 
 
     def find_path(self, start, end):
-        #print('find_path ',start,end)
+        "finds a path from start to end (which are postions as pairs)"
         return find_path(self.grid, tuple(start), tuple(end), self.p_moves)
-
--- notebooks/jens/supermarket.py
+++ notebooks/jens/supermarket.py
@@ -1,19 +1,20 @@
-from PIL import Image
+"supermarket simulations using Monte-Carlo and Markov-chains"
+import time
+import argparse
+import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import random
+from PIL import Image
+from a_star_adapted import AstarSupermarket
 from customer import Customer
-from copy import copy
-import time
-import argparse
-from a_star_adapted import A_star_supermarket
 
 try:
     import vlc
-    with_audio = True
+    WITH_AUDIO = True
 except ModuleNotFoundError:
-    with_audio = False
+    WITH_AUDIO = False
+
 
 #positon of shelves
 positions = {'dairy': 2, 'spices':6, 'fruit': 10, 'drinks':14}
@@ -24,22 +25,28 @@ icon_positions = [4, 1, 2, 3]
 #revenue
 revenue =  {'dairy': 5, 'spices':3, 'fruit': 4, 'drinks':6}
 
-#probality for not creating a customer now
-proba = 0.8
+#PROBAlity for not creating a customer now
+PROBA = 0.8
+
+#tile size
+TILE_SIZE = 32
 
 class SuperMarket:
+    """
+    supermarket that allows Monte-Carlo simulations
+    """
     def __init__(self, data_dir, opening, closing, astar):
         """
         contruct a supermarket with image files in data_dir
         opening and closing hours are integer (0..23)
         """
-        im = Image.open(data_dir+'/img/supermarket.png')
-        self.empty_market = np.array(im)
-        self.market = copy(self.empty_market)
-        im2 = Image.open(data_dir+'/img/tiles.png')
-        self.tiles = np.array(im2)
-        im3 = Image.open(data_dir+'/img/supermarket-icons.png')
-        self.icons = np.array(im3)
+        l_im = Image.open(data_dir+'/img/supermarket.png')
+        self.empty_market = np.array(l_im)
+        self.market = self.empty_market.copy()
+        l_im2 = Image.open(data_dir+'/img/tiles.png')
+        self.tiles = np.array(l_im2)
+        l_im3 = Image.open(data_dir+'/img/supermarket-icons.png')
+        self.icons = np.array(l_im3)
         self.opening = opening
         self.closing = closing
         self.do_astar = astar
@@ -49,83 +56,75 @@ class SuperMarket:
         self.transition = pd.read_csv(data_dir+'/data/freq_table.csv', index_col=0)
         self.income = 0
         self.audio = '/home/jkrause/Downloads/kasse.mp3'
-        self.astar = A_star_supermarket()
+        self.astar = AstarSupermarket()
 
     def show(self):
         "show the scene in matplotlig window"
-        fig = plt.gcf()
-        fig.clear()
+        l_fig = plt.gcf()
+        l_fig.clear()
         plt.imshow(self.market)
         plt.axis('off')
         plt.title(self.__repr__())
-        fig.canvas.draw()
+        l_fig.canvas.draw()
         time.sleep(0.001)
 
-    def get_icon(self, x):
+    def get_icon(self, xy_pos):
         "extract an icon at x (a coordinate pair)"
-        step=32
-        x0 = x[0]*step
-        x1 = x[1]*step
-        return self.icons[x0:x0+step, x1:x1+step]
+        x_0 = xy_pos[0]*TILE_SIZE
+        x_1 = xy_pos[1]*TILE_SIZE
+        return self.icons[x_0:x_0+TILE_SIZE, x_1:x_1+TILE_SIZE]
 
-    def get_tile(self, x):
+    def get_tile(self, xy_pos):
         "extract a tile at x (a coordinate pair)"
-        step=32
-        x0 = x[0]*step
-        x1 = x[1]*step
-        return self.tiles[x0:x0+step, x1:x1+step]
+        x_0 = xy_pos[0]*TILE_SIZE
+        x_1 = xy_pos[1]*TILE_SIZE
+        return self.tiles[x_0:x_0+TILE_SIZE, x_1:x_1+TILE_SIZE]
 
-    def set_tile(self, img, x):
+    def set_tile(self, img, xy_pos):
         "places a tile into img (numpy-array) at x (a coordinate pair)"
-        step=32
-        x0 = int(x[0]*step)
-        x1 = int(x[1]*step)
-        self.market[x0:x0+step, x1:x1+step] = img
+        x_0 = int(xy_pos[0]*TILE_SIZE)
+        x_1 = int(xy_pos[1]*TILE_SIZE)
+        self.market[x_0:x_0+TILE_SIZE, x_1:x_1+TILE_SIZE] = img
 
     def compose(self, places, tiles):
-        """places some icons in subermaket, 
+        """places some icons in subermaket,
         boths args a lists of pairs
         'places' are positons in supermarket (dest)
         'tiles' are postions in tiles images (src)
         """
-        self.market = copy(self.empty_market)
+        self.market = self.empty_market.copy()
         for place,tile in zip(places ,tiles):
             item = self.get_icon(tile)
             self.set_tile(item, place)
-        return None
 
     def sliding_compose(self, oldplaces, places, tiles):
         "slowly move costomer to new location"
         np_oldplaces = np.array(oldplaces, float)
         np_places = np.array(places, float)
-        
-        for t in np.linspace(0.,1.,16):
-            new_places = (1-t)*np_oldplaces + t*np_places
-            #print(f'tmp({t})=', new_places)
+
+        for time_step in np.linspace(0.,1.,16):
+            new_places = (1-time_step)*np_oldplaces + time_step*np_places
             self.compose(new_places, tiles)
             self.show()
-            #time.sleep(0.2)
-        return None
- 
+
     def astar_compose(self, oldplaces, places, tiles):
         "move costomers to new location using a* algorithm"
         path_lst = []
-        for s,f in zip(oldplaces, places):
-            p = self.astar.find_path(s,f)
-            path_lst.append(p)
-            
+        for l_start, l_finish in zip(oldplaces, places):
+            l_path = self.astar.find_path(l_start, l_finish)
+            path_lst.append(l_path)
+
         max_len = np.max([len(p) for p in path_lst])
         #print(f'max path = {max_len}')
         for i in range(max_len):
             new_places = []
-            for p in path_lst:
-                if i<len(p):
-                    new_places.append(p[i])
+            for l_path in path_lst:
+                if i<len(l_path):
+                    new_places.append(l_path[i])
                 else:
-                    new_places.append(p[-1])
+                    new_places.append(l_path[-1])
             self.compose(new_places, tiles)
             self.show()
-        return None
 
     def show_customers(self):
         "place (wo)man icon in correct location, some randomness"
@@ -135,21 +134,21 @@ class SuperMarket:
         for customer in self.customers:
             if customer.state == 'out':
                 continue
-            oldplaces.append([customer.x,customer.y])
+            oldplaces.append([customer.pos_x,customer.pos_y])
             if customer.state == 'in':
-                customer.x = 11
-                customer.y = 14+random.randint(0,1)
-                oldplaces[-1] = [customer.x,customer.y]
+                customer.pos_x = 11
+                customer.pos_y = 14+random.randint(0,1)
+                oldplaces[-1] = [customer.pos_x,customer.pos_y]
             elif customer.state == 'checkout':
-                customer.x = 8
-                customer.y = 3+4*+random.randint(0,2)
+                customer.pos_x = 8
+                customer.pos_y = 3+4*+random.randint(0,2)
             else:
-                customer.y = positions.get(customer.state,0)+random.randint(0,1)
-                customer.x = 2+random.randint(0,4)
-            places.append([customer.x,customer.y])
+                customer.pos_y = positions.get(customer.state,0)+random.randint(0,1)
+                customer.pos_x = 2+random.randint(0,4)
+            places.append([customer.pos_x,customer.pos_y])
             icons.append([0,0])
-        if(len(self.customers)>0):
-            if(self.do_astar==1):
+        if len(self.customers)>0:
+            if self.do_astar==1:
                 self.astar_compose(oldplaces, places, icons)
             else:
                 self.sliding_compose(oldplaces, places, icons)
@@ -162,11 +161,11 @@ class SuperMarket:
         places = []
         icons = []
         for place_shop, place_icon in zip(positions.values(), icon_positions):
-            for x in [-1,2]:
-                for y in range(5):
-                    if(random.random()<0.05):
+            for l_x in [-1,2]:
+                for l_y in range(5):
+                    if random.random()<0.05:
                         continue
-                    places.append([2+y, place_shop+x])
+                    places.append([2+l_y, place_shop+l_x])
                     icons.append([0, place_icon])
         self.compose(places, icons)
         self.show()
@@ -175,8 +174,8 @@ class SuperMarket:
     def next_minute(self):
         "move one minute in time"
         self.minutes += 1
-        for x in self.customers:
-            x.next_state()
+        for l_cust in self.customers:
+            l_cust.next_state()
 
     def is_open(self):
         "return False after closing time"
@@ -185,19 +184,22 @@ class SuperMarket:
     def remove_exitsting_customers(self):
         "remove inactive cosumers from the list"
         new_customer_list = []
-        for x in self.customers:
-            if(x.is_active()):
-                new_customer_list.append(x)
+        for l_cust in self.customers:
+            if l_cust.is_active():
+                new_customer_list.append(l_cust)
             else:
-                self.income += x.cost
-                if(with_audio):
-                    vlc.MediaPlayer(self.audio).play()
+                self.income += l_cust.cost
+                #try:
+                #    if WITH_AUDIO:
+                #        vlc.MediaPlayer(self.audio).play()
+                #except FileNotFoundError:
+                #    pass
         self.customers = new_customer_list
 
     def add_new_customers(self):
         "randomly add customers to the shop"
         rnd = random.random()
-        if(rnd>proba):
+        if rnd>PROBA:
             self.customers.append(Customer(self.nextid, self.transition, revenue))
             self.nextid += 1
 
@@ -208,26 +210,26 @@ class SuperMarket:
         return f'{self.opening+hour:02}:{minutes:02}'
 
     def __repr__(self):
-        "returns a nice string for printing"        
+        "returns a nice string for printing"
         return f'time={self.get_time()}; #customers={len(self.customers)}; income={self.income}'
 
-    def append_state_to_file(self, fn):
+    def append_state_to_file(self, f_name):
         """
-        append the state of the current customers to a csv file 
+        append the state of the current customers to a csv file
         at start of simulation file is truncated
         """
         if self.minutes ==0:
             mode = 'w'
         else:
             mode = 'a'
-        with open(fn, mode)  as fp:
-            if(self.minutes==0):
-                fp.write('time;customer;location\n')
+        with open(f_name, mode)  as file_p:
+            if self.minutes==0:
+                file_p.write('time;customer;location\n')
             for customer in self.customers:
-                fp.write(f'{self.get_time()};{customer.id};{customer.state}\n')
-        return None
+                file_p.write(f'{self.get_time()};{customer.cust_id};{customer.state}\n')
 
-if(__name__=='__main__'):
+
+if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Supermarket simulator')
     parser.add_argument('-g', '--grafic', help='with graphics',
                         type=int, default = 1, choices=[0,1])
@@ -235,29 +237,27 @@ if(__name__=='__main__'):
                         type=int, default = 1, choices=[0,1])
     args = parser.parse_args()
 
-    outfile = 'simulation.csv'
+    OUT_FILE = 'simulation.csv'
     doodl = SuperMarket('.', 9, 17, args.astar)
 
-    if(args.grafic):
+    if args.grafic:
         fig = plt.gcf()
         fig.show()
         fig.canvas.draw()
     else:
-        with_audio = False
+        WITH_AUDIO = False
 
     doodl.fill_shelves()
     try:
-        while (doodl.is_open()):
-            doodl.append_state_to_file(outfile)
+        while doodl.is_open():
+            doodl.append_state_to_file(OUT_FILE)
             doodl.next_minute()
             doodl.remove_exitsting_customers()
             doodl.add_new_customers()
-            #print(doodl)
-            #if(doodl.minutes % 5 == 0):
-            if(args.grafic):
+            if args.grafic:
                 doodl.show_customers()
     except KeyboardInterrupt as ex:
-        print('Supmarket close due to illness!')
+        print('Supmarket closes due to illness!')
         #raise ex
-doodl.append_state_to_file(outfile)
+doodl.append_state_to_file(OUT_FILE)
 print(f'Supermarkt earned {doodl.income}')
